d02b1bf4842a25c94645822acca470baf8a7d225
chad <chad.nehemiah94@gmail.com>
2024-11-10 22:33:34 +0700

    update build config and generation

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,23 @@
 import { defineConfig } from 'tsup'
+import fs from 'fs-extra'
+import path from 'path'
 
 export default defineConfig({
-	entry: ['src/*.ts', 'src/**/*.ts'],
+	entry: [
+		'src/*.ts',
+		'src/**/*.ts',
+		'!src/template/**',
+		'!src/template/**/*'
+	],
 	format: ['esm', 'cjs'],
-	dts: true,
+	dts: false,
 	clean: true,
 	outDir: 'dist',
 	platform: 'node',
+	async onSuccess() {
+		await fs.copy(
+			path.join(__dirname, 'src/template'),
+			path.join(__dirname, 'dist', 'template')
+		)
+	},
 })
